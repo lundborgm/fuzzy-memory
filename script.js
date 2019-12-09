a111b80e2881696c597b4ceff2c475560d73fd17
Michaela Lundborg <michaelalundborg@hotmail.com>
2019-12-09 15:14:57 +0100

    update

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ function resetGame() {
 function countCards() {
     openedCards.push(firstCard, secondCard);
 
-    if (openedCards.length === 4) {
+    if (openedCards.length === 16) {
 
         console.log(openedCards);
 
